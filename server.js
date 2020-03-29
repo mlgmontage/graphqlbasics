f7100a88688a138819ca874865eba55d3b151613
Abay <kettleofrassel@gmail.com>
2020-03-28 22:52:52 -0700

    hello world example

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 const app = express();
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "helloworld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello, World"
+      }
+    })
+  })
+});
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true
+  })
+);
 
 app.listen(5000, () => console.log("Server is running..."));
